95b22f46322174769c745da50d7da7209c968238
kitiketov <roskets801@gmail.com>
2025-10-11 13:06:18 +0500

    init

---
+++ Vibik/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Vibik")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Vibik.Pages")]
---
+++ Vibik/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Vibik;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
