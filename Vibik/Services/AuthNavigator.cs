8ba6addf65b0fc09759c03950ac7c5bf8cc3be7b
kitiketov <roskets801@gmail.com>
2025-12-13 13:33:05 +0500

    убрал дедлок

--- Vibik/Services/AuthNavigator.cs
+++ Vibik/Services/AuthNavigator.cs
@@ -1,19 +1,23 @@
 using Core.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Maui.ApplicationModel;
 using Microsoft.Maui.Controls;
 using Microsoft.Maui.Storage;
 
 namespace Vibik.Services;
 
-public sealed class AuthNavigator(LoginPage loginPage) : IAuthNavigator
+public sealed class AuthNavigator(IServiceProvider serviceProvider) : IAuthNavigator
 {
     public async Task RedirectToLoginAsync()
     {
         await MainThread.InvokeOnMainThreadAsync(() =>
         {
             Preferences.Remove("current_user");
-            if (Application.Current is not null)
-                Application.Current.MainPage = new NavigationPage(loginPage);
+            var app = Application.Current;
+            if (app is null) return;
+
+            var loginPage = serviceProvider.GetRequiredService<LoginPage>();
+            app.MainPage = new NavigationPage(loginPage);
         });
     }
 }
