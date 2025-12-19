a983ffb93c8effefea9570c1fabebe088ebabc06
Arina Kiskina <ae.kiskina@gmail.com>
2025-12-19 15:39:43 +0500

    brown splash

--- Vibik/Platforms/Android/MainActivity.cs
+++ Vibik/Platforms/Android/MainActivity.cs
@@ -3,9 +3,16 @@ using Android.Content.PM;
 
 namespace Vibik;
 
-[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop,
-    ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode |
-                           ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+[Activity(
+    Theme = "@style/MySplashTheme",
+    MainLauncher = true,
+    LaunchMode = LaunchMode.SingleTop,
+    ConfigurationChanges = ConfigChanges.ScreenSize
+                           | ConfigChanges.Orientation
+                           | ConfigChanges.UiMode
+                           | ConfigChanges.ScreenLayout
+                           | ConfigChanges.SmallestScreenSize
+                           | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
 }
